a3be149e70ae9fc342a7432257f89c3aef6448d0
2032309 <2032309@etu.cegepjonquiere.ca>
2021-04-19 14:47:25 -0400

    TERMINÉ

--- src/ca/cegepjonquiere/simulohmatique/Main.java
+++ src/ca/cegepjonquiere/simulohmatique/Main.java
@@ -4,20 +4,30 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     public static void main(String[] args) {
 
-        System.out.println("""
-                Pour construire votre circuit veuillez procéder comme suit:
+        System.out.println(
+                "Pour construire votre circuit veuillez procéder comme suit:\n" +
 
-                Pour OUVRIR un circuit en SÉRIE --> (
-                Pour OUVRIR un circuit en PARALLÈLE --> [
-                Pour FERMER un circuit en SÉRIE --> )
-                Pour FERMER un circuit en PARALLÈLE --> ]
+                "\nPour " + ANSI_GREEN + "OUVRIR" + ANSI_RESET + " un circuit en" + ANSI_BLUE + " SÉRIE" + ANSI_RESET + " --> (" +
+                "\nPour " + ANSI_GREEN + "OUVRIR" + ANSI_RESET + " un circuit en" + ANSI_YELLOW + " PARALLÈLE" + ANSI_RESET + " --> [" +
+                "\nPour " + ANSI_RED + "FERMER" + ANSI_RESET + " un circuit en" + ANSI_BLUE + " SÉRIE" + ANSI_RESET + " --> )" +
+                "\nPour " + ANSI_RED + "FERMER" + ANSI_RESET + " un circuit en" + ANSI_YELLOW+ " PARALLÈLE" + ANSI_RESET + " --> ]\n" +
 
-                N'oubliez pas de séparer CHAQUE caractère par un ESPACE
-                Pour vous aider --> Code couleur résisteur: (NBROJVbMGLAo)
+                "\nN'oubliez pas de séparer " + ANSI_PURPLE + "CHAQUE" + ANSI_RESET + " caractère par un" + ANSI_CYAN + " ESPACE" + ANSI_RESET +
+                "\nPour vous aider --> Code couleur résisteur: (NBROJVbMGLAo)\n" +
 
-                Veuillez créer vos circuits avec vos résisteurs -->\s""");
+                "\nVeuillez créer vos circuits avec vos résisteurs -->\s)");
 
         Scanner sc = new Scanner(System.in);
 
